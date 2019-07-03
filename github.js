0188d575e066486dd7ffd45802876f655a63637b
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-03 03:08:04 -0400

    Added the entire url.

--- github.js
+++ github.js
@@ -7,6 +7,10 @@ class GitHub {
   }
 
   async getUser(user) {
-    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+    const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
   }
 }
