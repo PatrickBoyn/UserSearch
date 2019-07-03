16e0c8d442b789cc2ee1d292062b8f0c3f1fd753
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-03 02:59:54 -0400

    Added some comments.

--- github.js
+++ github.js
@@ -1,5 +1,7 @@
 class GitHub {
   constructor() {
+    // This is a public API, you can get your own key for free,
+    // You just need it if you are going to make a lot of calls to the server.
     this.client_id = '30d094971baa5a82230c';
     this.client_secret = '2340b9d2900a5c07e147a2407f34ea5575366f9e';
   }
