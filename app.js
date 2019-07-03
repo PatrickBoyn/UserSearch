1d62677736988dea181fd87f4ebc3d19ff2516bc
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-03 02:58:26 -0400

    Added a check for an empty string.

--- app.js
+++ app.js
@@ -2,4 +2,8 @@ const searchUser = document.getElementById('search-user');
 
 searchUser.addEventListener('keyup', e => {
   const userText = e.target.value;
+
+  if (userText !== '') {
+    console.log(userText);
+  }
 });
