38a8c9a02f1da6e779a519b6fea4157a8b2e1580
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-03 13:07:37 -0400

    Added the HTML that will show the user info.

--- ui.js
+++ ui.js
@@ -4,6 +4,21 @@ class UI {
   }
 
   showProfile(user) {
-    console.log(user);
+    `<div class="card card-body">
+        <div class="row">
+            <div class="col-1-of-3">
+                <img src="${user.avatar_url}">
+                <a href="${
+                  user.html_url
+                }" target="_blank" class="btn">View Profile</a>
+            </div>
+            <div class="col-1-of-9>
+               <span>Public Repos: ${user.public_repos}</span>
+               <span>Public Gists: ${user.public_gists}</span>
+               <span>Followers: ${user.followers}</span>
+               <span>Following: ${user.following}</span>
+            </div>
+        </div>
+    </div>`;
   }
 }
