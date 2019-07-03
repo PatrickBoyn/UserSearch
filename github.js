d046ef80276b56386384baf8d16316bbd7b43788
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-03 03:05:21 -0400

    Added an async method to get a users details.

--- github.js
+++ github.js
@@ -5,4 +5,8 @@ class GitHub {
     this.client_id = '30d094971baa5a82230c';
     this.client_secret = '2340b9d2900a5c07e147a2407f34ea5575366f9e';
   }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}`);
+  }
 }
