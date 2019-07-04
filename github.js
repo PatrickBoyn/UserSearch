0a74bf04784ac4b734b702ccf93198685dcf8c7a
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-04 12:58:36 -0400

    Added the repo url.  Also added new params for the link.

--- github.js
+++ github.js
@@ -4,10 +4,18 @@ class GitHub {
     // You just need it if you are going to make a lot of calls to the server.
     this.client_id = '30d094971baa5a82230c';
     this.client_secret = '2340b9d2900a5c07e147a2407f34ea5575366f9e';
+    this.repos_count = 5;
+    this.repos_sort = 'created: asc';
   }
 
   async getUser(user) {
     const profileResponse = await fetch(
+      `https://api.github.com/users/${user}?client_id=${
+        this.client_id
+      }&client_secret=${this.client_secret}`
+    );
+
+    const repoResponse = await fetch(
       `https://api.github.com/users/${user}?client_id=${
         this.client_id
       }&client_secret=${this.client_secret}`
