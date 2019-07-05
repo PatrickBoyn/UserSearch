a8a6d71b39e5430fcb65307af13d1f7c1ca92f3d
BLACKBOX\dakil <dakilangmanok@gmail.com>
2019-07-05 14:53:24 -0400

    Updated the new Secrets.

--- JavaScript/github.js
+++ JavaScript/github.js
@@ -1,9 +1,11 @@
+const secret = new Secrets();
+
 class GitHub {
   constructor() {
     // This is a public API, you can get your own key for free,
     // You just need it if you are going to make a lot of calls to the server.
-    this.client_id = '30d094971baa5a82230c';
-    this.client_secret = '2340b9d2900a5c07e147a2407f34ea5575366f9e';
+    this.client_id = secret.clientId();
+    this.client_secret = secret.clientSecret();
     this.repos_count = 10;
     this.repos_sort = 'created: asc';
   }
